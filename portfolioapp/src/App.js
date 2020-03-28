73e6df1b3fbe5f8b8e3be3071749dbd527c07e79
BrandonBrown <brandonbrown@pursuit.org>
2020-03-27 21:37:18 -0400

    create rouuter

---
+++ portfolioapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
