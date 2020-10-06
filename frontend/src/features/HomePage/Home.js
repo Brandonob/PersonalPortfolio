4ba149c7016ca4f707e3cd9f9b9c713909e9d2e8
BrandonBrown <brandonbrown@pursuit.org>
2020-10-05 23:28:24 -0400

    add images to app

--- frontend/src/features/HomePage/Home.js
+++ frontend/src/features/HomePage/Home.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import brickwallart from '../../images/brickwallart.jpg'
+import brandonImg from '../../images/brandonImg.png'
+import emptygallery from '../../images/emptygallery.jpg'
+import avatarframe from '../../images/avatarframe.png'
+
 
 const Home = () => {
     return (
-        <div>
-            {"Hi!"}
+        <div className="Body" >
+            
+            <div className="imageHeader" 
+                style={{ backgroundImage:`url(${emptygallery})`,
+                        border: "0",
+                        height: "90vh",
+                        margin: "0",
+                        display: "flex",
+                        padding: "0",
+                        overflow: "hidden",
+                        position: "relative",
+                        maxHeight: "1000px",
+                        alignItems: "center",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center center", }}>
+                <img alt="" src={brandonImg} style={{ height: "43vh",
+                                                        position: "absolute",
+                                                        right: "9%",
+                                                        top: "13%"    }}/>
+                <img alt="" src={avatarframe} style={{ height: "60vh",
+                                                        position: "absolute",
+                                                        right: "5%",
+                                                        top: "7%"    }}/>
+
+                
+
+            </div>
         </div>
     )
 }
