902aa0892e7064d2d7634cdff0b3ebe769449b9f
BrandonBrown <brandonbrown@pursuit.org>
2025-01-26 11:33:44 -0500

    update portfolio to match new resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,32 +1,48 @@
 import React, { Component } from 'react';
-import cssIcon from '../Components/skillimages/cssIcon.png'
-import firebaseIcon from '../Components/skillimages/firebaseIcon.png'
-import gitIcon from '../Components/skillimages/gitIcon.png'
-import htmlIcon from '../Components/skillimages/htmlIcon.png'
-import javascriptIcon from '../Components/skillimages/javascriptIcon.png'
-import nodejsIcon from '../Components/skillimages/nodejsIcon.png'
-import npmIcon from '../Components/skillimages/npmIcon.png'
-import postgresqlIcon from '../Components/skillimages/postgresqlIcon.png'
-import reactIcon from '../Components/skillimages/reactIcon.png'
-import reduxIcon from '../Components/skillimages/reduxIcon.png'
-import herokuIcon from '../Components/skillimages/herokuIcon.png'
+import cssIcon from '../Components/skillimages/cssIcon.png';
+import firebaseIcon from '../Components/skillimages/firebaseIcon.png';
+import gitIcon from '../Components/skillimages/gitIcon.png';
+import htmlIcon from '../Components/skillimages/htmlIcon.png';
+import javascriptIcon from '../Components/skillimages/javascriptIcon.png';
+import nodejsIcon from '../Components/skillimages/nodejsIcon.png';
+import npmIcon from '../Components/skillimages/npmIcon.png';
+import postgresqlIcon from '../Components/skillimages/postgresqlIcon.png';
+import reactIcon from '../Components/skillimages/reactIcon.png';
+import reduxIcon from '../Components/skillimages/reduxIcon.png';
+import herokuIcon from '../Components/skillimages/herokuIcon.png';
+import rubyIcon from '../Components/skillimages/ruby.png';
+import tailwindIcon from '../Components/skillimages/tailwind.png';
+import typescriptIcon from '../Components/skillimages/typescript.png';
+import dockerIcon from '../Components/skillimages/docker.png';
 
 class Resume extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      var education = this.props.data.education.map(function (education) {
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className='info'>
+              {education.degree} <span>&bull;</span>
+              <em className='date'>{education.graduated}</em>
+            </p>
+            <p>{education.description}</p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function (work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className='info'>
+              {work.title}
+              <span>&bull;</span> <em className='date'>{work.years}</em>
+            </p>
             <p>{work.description}</p>
-        </div>
-      })
+          </div>
+        );
+      });
       // var skills = this.props.data.skills.map(function(skills){
       //   var className = 'bar-expand '+skills.name.toLowerCase();
       //   return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
@@ -34,107 +50,173 @@ class Resume extends Component {
     }
 
     return (
-      <section id="resume">
-
-        <div className="row skill" style={{ paddingBottom: "50px",
-                                            borderBottom: "1px solid #E8E8E8",
-                                            marginBottom: "50px", }}>
-
-          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+      <section id='resume'>
+        <div
+          className='row skill'
+          style={{
+            paddingBottom: '50px',
+            borderBottom: '1px solid #E8E8E8',
+            marginBottom: '50px',
+          }}
+        >
+          <div className='three columns header-col'>
+            <h1>
+              <span>Skills</span>
+            </h1>
           </div>
 
-          <div className="nine columns main-col">
+          <div className='nine columns main-col'>
+            <p>{skillmessage}</p>
 
-            <p>{skillmessage}
-            </p>
-
-            <div className="skillsImgs">
-              
+            <div className='skillsImgs'>
               <div>
-                <img alt="" src={htmlIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={htmlIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>HTML</h1>
-              </div> 
+              </div>
               <div>
-                <img alt="" src={cssIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={cssIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>CSS</h1>
               </div>
               <div>
-                <img alt="" src={javascriptIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={javascriptIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>Javascript</h1>
               </div>
               <div>
-                <img alt="" src={postgresqlIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={postgresqlIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>PostgreSQL</h1>
               </div>
               <div>
-                <img alt="" src={nodejsIcon} style={{width: "140px", height: "140px"}}/>
+                <img
+                  alt=''
+                  src={nodejsIcon}
+                  style={{ width: '140px', height: '140px' }}
+                />
                 <h1>Nodejs</h1>
               </div>
               <div>
-                <img alt="" src={gitIcon} style={{width: "120px", height: "120px"}}/>
-                <h1 style={{ marginTop: "15px" }}>Git</h1>
+                <img
+                  alt=''
+                  src={gitIcon}
+                  style={{ width: '120px', height: '120px' }}
+                />
+                <h1 style={{ marginTop: '15px' }}>Git</h1>
               </div>
               <div>
-                <img alt="" src={reactIcon} style={{width: "135px", height: "135px"}}/>
+                <img
+                  alt=''
+                  src={reactIcon}
+                  style={{ width: '135px', height: '135px' }}
+                />
                 <h1>React</h1>
               </div>
               <div>
-                <img alt="" src={reduxIcon} style={{width: "110px", height: "110px"}}/>
-                <h1 style={{ marginTop: "19px" }}>Redux</h1>
+                <img
+                  alt=''
+                  src={reduxIcon}
+                  style={{ width: '110px', height: '110px' }}
+                />
+                <h1 style={{ marginTop: '19px' }}>Redux</h1>
               </div>
               <div>
-                <img alt="" src={npmIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={npmIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>NPM</h1>
               </div>
               <div>
-                <img alt="" src={firebaseIcon} style={{width: "125px", height: "125px"}}/>
+                <img
+                  alt=''
+                  src={firebaseIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
                 <h1>Firebase</h1>
               </div>
               <div>
-                <img alt="" src={herokuIcon} style={{width: "110px", height: "110px"}}/>
-                <h1 style={{ marginTop: "10px" }}>Heroku</h1>
+                <img
+                  alt=''
+                  src={herokuIcon}
+                  style={{ width: '110px', height: '110px' }}
+                />
+                <h1 style={{ marginTop: '10px' }}>Heroku</h1>
               </div>
-
+              <div>
+                <img
+                  alt=''
+                  src={rubyIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
+                <h1>Ruby</h1>
+              </div>
+              <div>
+                <img
+                  alt=''
+                  src={tailwindIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
+                <h1>Tailwind CSS</h1>
+              </div>
+              <div>
+                <img
+                  alt=''
+                  src={typescriptIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
+                <h1>TypeScript</h1>
+              </div>
+              <div>
+                <img
+                  alt=''
+                  src={dockerIcon}
+                  style={{ width: '125px', height: '125px' }}
+                />
+                <h1>Docker</h1>
+              </div>
             </div>
           </div>
         </div>
 
+        <div className='row education'>
+          <div className='three columns header-col'>
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
 
-
-
-        
-
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+          <div className='nine columns main-col'>
+            <div className='row item'>
+              <div className='twelve columns'>{education}</div>
             </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+          </div>
         </div>
-    </div>
 
+        <div className='row work'>
+          <div className='three columns header-col'>
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
-
-      
-   </section>
+          <div className='nine columns main-col'>{work}</div>
+        </div>
+      </section>
     );
   }
 }
